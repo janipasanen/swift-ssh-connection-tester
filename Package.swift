96957214501af2a925b800fe24d9625bfbfd6c37
Jani Pasanen <jani.m.pasanen@gmail.com>
2024-05-28 14:37:04 +0200

    swift to swift 5.3 swift-tools, addedd NIOSSH as dependency and added the sample ssh client code to the the project.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,24 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-ssh-connection-tester",
+    platforms: [
+            .macOS(.v10_15)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio-ssh/", from: "0.3.3"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "swift-ssh-connection-tester"),
+        .target(
+            name: "swift-ssh-connection-tester",
+            dependencies: [
+                .product(name: "NIOSSH", package: "swift-nio-ssh"),
+            ]
+        ),
     ]
 )
